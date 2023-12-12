fc7ffa67f9454d34445308fb7321e48f97fed37a
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-12 10:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 const currUrl = window.location.href ;
 const urlLen = currUrl.length ;
-alert(`The lenght of the URL is: ${urlLen}`);
+alert(`The length of the URL is: ${urlLen}`);
